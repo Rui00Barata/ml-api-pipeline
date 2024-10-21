51b799e796e3994f29991dd4463570561d8475f6
Rui00Barata <rui.barata@ubi.pt>
2024-10-21 21:07:56 +0100

    Update YOLO integration to return annotated images

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-# src/main.py
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse
+from src.detector import ObjectDetector
+import shutil
 
 app = FastAPI()
+detector = ObjectDetector()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the ML API Pipeline"}
+
+@app.post("/detect/")
+async def detect(file: UploadFile = File(...)):
+    with open(f"temp_{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # annotated_image_path = detector.detect_objects(f"/tmp/temp_{file.filename}")
+    annotated_image_path = detector.detect_objects(f"temp_{file.filename}")
+    if annotated_image_path:
+        return FileResponse(annotated_image_path)
+    else:
+        raise HTTPException(status_code=404, detail="No person detected in the image.")
